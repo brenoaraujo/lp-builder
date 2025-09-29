bc1b9a14e246f475c18d252aca9ee9ecb7dfafc1
Breno Araújo <brenoaraujobh@gmail.com>
2025-09-29 16:32:31 -0700

    footer adjustments

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -17,8 +17,6 @@ function FooterPrimitive({ data }) {
       "About Us",
       "Rules of Play",
       "Winners",
-      "Share Feedback",
-      "Partners",
       "FAQs",
       "Contact Us",
       "Privacy Policy",
@@ -76,7 +74,7 @@ function FooterPrimitive({ data }) {
             </div>
           </div>
         </div>
-
+{/* BC Rules 
         <div className="self-stretch outline outline-1 border-zinc-900 flex flex-col justify-start items-start" data-display="no" data-label="BC Rules">
           <div className="self-stretch p-2.5 bg-white inline-flex justify-between items-center overflow-hidden">
             <div className="justify-start text-black text-sm font-normal leading-relaxed" >
@@ -97,15 +95,15 @@ function FooterPrimitive({ data }) {
               {ageText}
             </div>
           </div>
-        </div>
-
+        </div>*/}
+{/* Ontario Rules
         <div className="self-stretch px-60 py-6 bg-Colors-alt-background flex flex-col justify-start items-center gap-2" data-display="yes" data-label="ON Rules">
           <div className="flex flex-col justify-start items-start gap-2.5">
             <div className="text-center justify-start text-Colors-alt-foreground text-sm font-normal leading-tight" >
               {responsabilityText}
             </div>
           </div>
-        </div>
+        </div>*/}
 
         <div className="self-stretch inline-flex justify-between items-center">
           <div className="flex-1 inline-flex flex-col justify-start items-start gap-4">
@@ -124,6 +122,7 @@ function FooterPrimitive({ data }) {
                 ))}
               </div>
             </div>
+            {/* Language
             <div data-description="false" data-label="false" data-state="Default" className="w-32 flex flex-col justify-start items-start gap-2" >
               <div className="self-stretch h-10 px-4 py-2 bg-Colors-background rounded-md outline outline-1 outline-offset-[-1px] bg-Colors-muted-background inline-flex justify-start items-center gap-2 overflow-hidden">
                 <div className="flex-1 justify-start text-Colors-foreground text-sm font-medium leading-tight"  >
@@ -134,7 +133,7 @@ function FooterPrimitive({ data }) {
                   <ChevronDown />
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
 
           <div className="inline-flex flex-col justify-start items-start gap-4">
